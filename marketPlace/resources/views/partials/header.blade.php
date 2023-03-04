515eed6bb864b00a33f2ed7316f62e04aa12a577
Jaume Cadafalch <84527588+jcadafalch@users.noreply.github.com>
2023-03-04 21:19:22 +0100

    header & footer created in layout.html

---
+++ marketPlace/resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+    <div>
+        <img src="" alt="">
+    </div>
+    <div class="buscador">
+        <div class="buscadorContainer">
+            <select id="selectCategories">
+                <option value="">Totes les categories</option>
+            </select>
+            <input type="search" name="" id="">
+            <button></button>
+        </div>
+    </div>
+    <div class="login">
+        <button>Registra't / Inicia Sessió</button>
+    </div>
+    <div class="cartAndProfile">
+        <span class="material-symbols-outlined">
+            shopping_cart
+        </span>
+        <span class="material-symbols-outlined">
+            person
+        </span>
+    </div>
+</header>
