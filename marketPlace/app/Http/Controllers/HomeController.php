bececd9427f40c0d51eafedfc76dc072e30ee688
Mariam Pipoyan <pipoyan.mariam@alumnat.copernic.cat>
2023-03-06 16:33:43 +0100

    added Home page controller and configurate route

--- marketPlace/app/Http/Controllers/HomeController.php
+++ marketPlace/app/Http/Controllers/HomeController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index' /*, ['products' => Product::all()]*/);
+        return view('home.index' , ['products' => Product::all()]);
     }
 }
