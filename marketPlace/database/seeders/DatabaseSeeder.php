30dd486c17d60e18782c2fcaf6837075a778dbd7
Daniel rodriguez saez <rodriguez.saez.daniel@alumnat.copernic.cat>
2023-03-07 19:48:27 +0100

    Añadir creación de categorias a mano, a traves de un array

--- marketPlace/database/seeders/DatabaseSeeder.php
+++ marketPlace/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -10,20 +12,49 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+
+    // Array de categories
+     public $Categoris = [
+        1 => [
+            'name' => 'Moda',
+        ],
+        2 => [
+            'name' => 'Accesoris',
+        ],
+        3 => [
+            'name' => 'Fornitures',
+        ],
+        4 =>[
+            'name' => 'Toys',
+        ],
+        5 =>[
+            'name' => 'Art',
+        ]
+    ];
     public function run(): void
     {
+     
+    
         // Creem 5 productes aleatoris 
-        \App\Models\Product::factory(5)->create();
+        Product::factory(5)->create();
           
         // Creem 5 categories aleatoris
-        $Category = \App\Models\Category::factory(5)->create();
+        //$Category = \App\Models\Category::factory(5)->create();
+        
+        // Proves per inserta categories a mà
+        foreach ($this->Categoris as $c) {
+            $category = new Category();
+            $category->name = $c['name'];
+            $category->save();
+        }
+
         
         // Agafem tots el productes que hem creat
-        $allProducts = \App\Models\Category::all();
+        $allProducts = Category::all();
      
         foreach ($allProducts as $p) {
         // Fem la unio
-        $p->products()->attach(\App\Models\Category::find($p->id));
+        $p->products()->attach(Category::find($p->id));
         }
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
