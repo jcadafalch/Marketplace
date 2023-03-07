f6b6e89701d777454000fd4f602fcc91993766a0
Daniel rodriguez saez <rodriguez.saez.daniel@alumnat.copernic.cat>
2023-03-07 19:31:12 +0100

    Añadir permisos para poder crear Categories y Products, pasando todos los parametros

--- marketPlace/app/Models/Product.php
+++ marketPlace/app/Models/Product.php
@@ -11,7 +11,8 @@ class Product extends Model
     protected $fillable = [
         'name',
         'description',
-        'selled_at',
+        'price',
+        'url'
     ];
 
     public function categories() {
