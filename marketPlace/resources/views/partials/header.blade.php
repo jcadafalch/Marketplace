259e9c0fad278f07f5747f7f9f55c0cab4d2221b
Mariam Pipoyan <pipoyan.mariam@alumnat.copernic.cat>
2023-03-08 03:43:51 +0100

    hotfix

---
+++ marketPlace/resources/views/partials/header.blade.php
@@ -0,0 +1,33 @@
+<header>
+    <div class="logoHeader">
+        <img src="{{asset('images/LogoFooter.png')}}" alt="Logo" />
+    </div>
+
+    <div class="headerOptions">
+        <div class="buscador">
+            <select id="selectCategories">
+                <option value="">Totes les categories</option>
+                <option value="">Totes les categories</option>
+                <option value="">Totes les categories</option>
+            </select>
+            <input type="search" name="" id="search" placeholder="Introduce el articulo que quieres buscar" />
+            <span class="material-symbols-rounded" id="headerButtonSearch">
+                search
+            </span>
+        </div>
+        <div class="login">
+            <button>Registra't / Inicia Sessió</button>
+        </div>
+        <div class="cartAndProfile" style="display: none">
+            <div>
+                <span class="material-symbols-outlined">
+                    shopping_cart
+                </span>
+            </div>
+
+            <div>
+                <span class="material-symbols-outlined"> person </span>
+            </div>
+        </div>
+    </div>
+</header>
