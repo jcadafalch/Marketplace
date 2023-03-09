2b7e616b6a1bf65ffcfcc272ae87cbe330b803c1
Jaume Cadafalch <84527588+jcadafalch@users.noreply.github.com>
2023-03-09 17:38:35 +0100

    header i footer acabat a niveel html i css

--- marketPlace/resources/views/partials/footer.blade.php
+++ marketPlace/resources/views/partials/footer.blade.php
@@ -1,6 +1,12 @@
 <footer>
     <div class="footer-content">
-        <img src="/marketPlace/resources/images/LogoFooter.png" alt="Logo" />
-        <p>&copy; Copyright 2023 Craft Made</p>
+        <img src="{{ asset('/images/LogoFooter.png') }}" alt="Logo" />
+        <div class="creators">
+            <p>Mariam Pipoyan Paronyan</p>
+            <p>Daniel Rodríguez Saez</p>
+            <p>Jaume Cadafalch Miró</p>
+            <p>Adrián Ramírez Masagú</p>
+        </div>
+        <p class="copyright">&copy; Copyright 2023 Craft Made</p>
     </div>
 </footer>
--- marketPlace/resources/views/partials/header.blade.php
+++ marketPlace/resources/views/partials/header.blade.php
@@ -1,33 +1,36 @@
 <header>
     <div class="logoHeader">
-        <img src="/marketPlace/resources/images/LogoFooter.png" alt="Logo" />
+        <img src="{{ asset('/images/LogoFooter.png') }}" alt="Logo" />
     </div>
 
-    <div class="headerOptions">
-        <div class="buscador">
-            <select id="selectCategories">
-                <option value="">Totes les categories</option>
-                <option value="">Totes les categories</option>
-                <option value="">Totes les categories</option>
-            </select>
-            <input type="search" name="" id="search" placeholder="Introduce el articulo que quieres buscar" />
-            <span class="material-symbols-rounded" id="headerButtonSearch">
-                search
+    <div class="buscador">
+        <select id="selectCategories">
+            <option value="">Todas las categorías</option>
+            <option value="">Todas las categorías</option>
+            <option value="">Todas las categorías</option>
+        </select>
+        <input type="search" name="" id="search" placeholder="Introduce el articulo que quieres buscar" />
+        <span class="material-symbols-rounded" id="headerButtonSearch">
+            search
+        </span>
+    </div>
+
+    <div class="cartAndProfile">
+        <div class="displayIcon">
+            <p id="numberOfProducts">0</p>
+            <span class="material-symbols-outlined shoppingCart">
+                shopping_cart
             </span>
         </div>
-        <div class="login">
-            <button>Registra't / Inicia Sessió</button>
-        </div>
-        <div class="cartAndProfile" style="display: none">
-            <div>
-                <span class="material-symbols-outlined">
-                    shopping_cart
-                </span>
-            </div>
 
-            <div>
-                <span class="material-symbols-outlined"> person </span>
-            </div>
+        <div class="displayIcon user" style="display: none">
+            <span class="material-symbols-outlined"> person </span>
+            <p>username</p>
+        </div>
+        <div class="login">
+            <button>
+                <a href="{{ redirect('/login') }}">Regístrate / Inicia Sesión</a>
+            </button>
         </div>
     </div>
 </header>
