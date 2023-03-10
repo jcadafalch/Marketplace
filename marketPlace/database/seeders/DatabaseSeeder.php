03268023d34d17ad6ee18ce4602e763ddafe71ed
Dani Rodriguez Saez <>
2023-03-10 23:31:57 +0100

    Reestructuració dataBaseSeeder, amb Modulació. Incorpora id de categoria aleatòria. Mostrar logs d'Execució exitosa de la inserció de dades a cada taula.

--- marketPlace/database/seeders/DatabaseSeeder.php
+++ marketPlace/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,52 +14,76 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
 
-    // Array de categories
-     public $Categoris = [
-        1 => [
-            'name' => 'Moda',
-        ],
-        2 => [
-            'name' => 'Accesoris',
-        ],
-        3 => [
-            'name' => 'Fornitures',
-        ],
-        4 =>[
-            'name' => 'Toys',
-        ],
-        5 =>[
-            'name' => 'Art',
-        ]
-    ];
     public function run(): void
-    {
-     
+    { 
+    // Productes a generar    
+    $numProducts = 20;
+    // Categories a generar
+    $numCategories = 20;
     
-        // Creem 5 productes aleatoris 
-        Product::factory(5)->create();
-          
+    
+    self::generateProducts($numProducts);
+    $this->command->info('Taula productes inicialitzada amb èxit');   
+    self::generateCategories($numCategories);
+    $this->command->info('Taula categories inicialitzada amb èxit');
+    self::attachProductCategories();
+    $this->command->info('Taula del mitg inicialitzada amb èxit');
+    }
+
+    private static function generateProducts($numProducts){
+        // Eliminem les dades de la taula 
+        DB::table('products')->delete();
+
+         // Creem 5 productes aleatoris 
+         Product::factory($numProducts)->create();
+    }
+
+    private static function generateCategories($numCategories){
+        // Eliminem les dades de la taula 
+        DB::table('categories')->delete();
+        
+        // Array de categories
+        $Categoris = [
+            1 => [
+                'name' => 'Moda',
+            ],
+            2 => [
+                'name' => 'Accesoris',
+            ],
+            3 => [
+                'name' => 'Fornitures',
+            ],
+            4 =>[
+                'name' => 'Toys',
+            ],
+            5 =>[
+                'name' => 'Art',
+            ],
+        ];
+        
         // Creem 5 categories aleatoris
-        //$Category = \App\Models\Category::factory(5)->create();
+        Category::factory($numCategories)->create();
         
         // Proves per inserta categories a mà
-        foreach ($this->Categoris as $c) {
+        /*foreach ($Categoris as $c) {
             $category = new Category();
             $category->name = $c['name'];
             $category->save();
-        }
+        }*/
+    }
 
+    private static function attachProductCategories(){
+        // Eliminem les dades de la taula 
+        DB::table('category_product')->delete();
         
         // Agafem tots el productes que hem creat
         $allProducts = Category::all();
-     
+        $numCategores = Category::count();
+        
         foreach ($allProducts as $p) {
+        $randomCategory = rand(1, $numCategores);
         // Fem la unio
-        $p->products()->attach(Category::find($p->id));
+         $p->products()->attach(Category::find($randomCategory));
         }
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
