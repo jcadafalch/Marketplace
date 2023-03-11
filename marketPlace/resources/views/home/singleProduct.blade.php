35be9c3c7cf05d73f8cbc8f25c3428a2a1ec78bb
Mariam Pipoyan <pipoyan.mariam@alumnat.copernic.cat>
2023-03-11 18:56:00 +0100

    modificado estructura scss - añadido _buttons.scss

--- marketPlace/resources/views/home/singleProduct.blade.php
+++ marketPlace/resources/views/home/singleProduct.blade.php
@@ -5,11 +5,11 @@
 @section('content')
 
     <div>
-        <img src="{{ asset('storage/img/' . $product->url) }}" />
+        <img class="product-image" src="{{ asset('storage/img/' . $product->url) }}" />
 
-        <h4>{{ $product->name }}</h4>
-        <p>{{ $product->description }}</p>
-        <p>{{ $product->price }}</p>
+        <p class="product-name">{{ $product->name }}</p>
+        <p class="product-description">{{ $product->description }}</p>
+        <p class="product-price">{{ $product->price }}</p>
 
     </div>
 
