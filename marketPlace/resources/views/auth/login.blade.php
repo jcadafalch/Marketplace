2797f3cd055d13cbf1efba28fc13c3f598b5f83e
Mariam Pipoyan <pipoyan.mariam@alumnat.copernic.cat>
2023-03-12 21:47:36 +0100

    añadido enlace hacia HomePage en Logo

--- marketPlace/resources/views/auth/login.blade.php
+++ marketPlace/resources/views/auth/login.blade.php
@@ -1,24 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LogIn</title>
 </head>
+
 <body>
-    <h1>Login Page</h1>
+    <div class="logoHeader">
+        <img src="{{ asset('/images/LogoFooter.png') }}" alt="Logo" />
+    </div>
+    <h1>Craft Made</h1>
     <div>
         <form>
             <label>Name</label>
             <p><input type="text" name="name"></p>
-            
+
             <label>Mail</label>
             <p><input type="mail" name="mail"></p>
-            
+
             <label>Password</label>
             <p><input type="password" name="password"></p>
-            
+
             <label>Repeat password</label>
             <p><input type="password" name="password"></p>
 
@@ -31,4 +36,5 @@
         </form>
     </div>
 </body>
+
 </html>
