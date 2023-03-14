63087a844ac5311ba102a387279c12f6009c2f28
Mariam Pipoyan <pipoyan.mariam@alumnat.copernic.cat>
2023-03-14 15:18:15 +0100

    guarda id de producto en cookie, button disabled

--- marketPlace/public/js/addToCart.js
+++ marketPlace/public/js/addToCart.js
@@ -2,13 +2,24 @@ const button = document.querySelectorAll('.button-addToCart');
 const contador = document.querySelector('#numberOfProducts');
 
 let counter = 1;
+const arrayProducts = [];
 
 button.forEach(element => {
     element.addEventListener('click', function (e) {
-        parseInt(contador.innerHTML = counter);
-        counter++;
+
+        if (!arrayProducts.includes(element.id)) {
+            parseInt(contador.innerHTML = counter);
+            counter++;
+            arrayProducts.push(element.id);
+
+            element.setAttribute('disabled', true);
+        }
+
+        document.cookie = "item =" + arrayProducts ;
     });
 });
+
+
 
 
 
