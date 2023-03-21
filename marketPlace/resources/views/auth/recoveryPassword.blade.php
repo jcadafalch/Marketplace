da19b8c70ef2aba93865789fa6bf51215255730e
Daniel rodriguez saez <rodriguez.saez.daniel@alumnat.copernic.cat>
2023-03-21 21:12:59 +0100

    Crear estructuras html per recuperar contrasenya

--- marketPlace/resources/views/auth/recoveryPassword.blade.php
+++ marketPlace/resources/views/auth/recoveryPassword.blade.php
@@ -3,9 +3,26 @@
 @section('title', 'Recuperar Contraseña')
 
 @section('content')
-
-<h1>hola entro</h1>
-
-
-
+<div class= "userForm">
+    <div class="userForm-title" >
+    <h1>Craft Made</h1>
+    </div>
+    <form class="userForm-form" method="">
+        @csrf
+        <div class="userForm-form-label">
+            <label>Ayuda de contraseña</label>
+        </div>
+        <div class="userForm-form-item">
+            <p>Dirección de e-mail</p>            
+            <input type="mail" name="mail">
+        </div>
+        <div class="userForm-form-help">
+            <p>Al enviar recibira un correo en un su email con todas las intrucciones,
+            <strong>Por favor verifique su bandeja de spam.</strong></p>
+        </div>
+        <div class="userForm-form-button">
+            <button class="button-recovery" type="submit">Enviar</button>
+        </div>      
+    </form>
+</div>
 @endsection
