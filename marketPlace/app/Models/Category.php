77c44dfec0ce0bf10baa6054d2fa0de5628cbbc8
Daniel rodriguez saez <rodriguez.saez.daniel@alumnat.copernic.cat>
2023-03-23 21:19:56 +0100

    Afegir metode per trobar les subcategories a la clase Categoriy

--- marketPlace/app/Models/Category.php
+++ marketPlace/app/Models/Category.php
@@ -16,4 +16,9 @@ class Category extends Model
     public function products() {
         return $this->belongsToMany(Product::class)->withTimeStamps();
     }
+
+    public static function getSubCategories($parentId){
+        return Category::select('name')
+        ->where('parent_id', $parentId)->get();
+    }
 }
