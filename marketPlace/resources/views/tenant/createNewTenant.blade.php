debd19ec27afbc229965bf7405305d44b2ea32c8
Mariam Pipoyan <pipoyan.mariam@alumnat.copernic.cat>
2023-03-23 17:18:01 +0100

    eliminado logo del formulario en la pagina 'crear nueva tienda'

--- marketPlace/resources/views/tenant/createNewTenant.blade.php
+++ marketPlace/resources/views/tenant/createNewTenant.blade.php
@@ -3,30 +3,30 @@
 @section('title', 'Recuperar Contraseña')
 
 @section('content')
-<div class= "userForm">
-    <div class="userForm-title" >
-    <h1>Craft Made</h1>
+    <div class="userForm">
+        <div class="userForm-title">
+            <div class="userForm-form-label">
+                <label>Dar de alta tu tienda</label>
+            </div>
+        </div>
+        <form class="userForm-form" method="">
+            @csrf
+
+            <div class="userForm-form-item">
+                <p>Nombre de la tienda</p>
+                <input type="text" name="shopName">
+            </div>
+            <div class="userForm-form-item">
+                <p>Nombre completo</p>
+                <input type="text" name="name">
+            </div>
+            <div class="userForm-form-item">
+                <p>Nif o Dni</p>
+                <input type="text" name="nif">
+            </div>
+            <div class="userForm-form-button">
+                <button class="button-form" type="submit">Enviar</button>
+            </div>
+        </form>
     </div>
-    <form class="userForm-form" method="">
-        @csrf
-        <div class="userForm-form-label">
-            <label>Dar de alta tu tienda</label>
-        </div>
-        <div class="userForm-form-item">
-            <p>Nombre de la tienda</p>
-            <input type="text" name="shopName">
-        </div>
-        <div class="userForm-form-item">
-            <p>Nombre completo</p>
-            <input type="text" name="name" >
-        </div>
-        <div class="userForm-form-item">
-            <p>Nif o Dni</p>            
-            <input type="text" name="nif">
-        </div>
-        <div class="userForm-form-button">
-            <button class="button-form" type="submit">Enviar</button>
-        </div>      
-    </form>
-</div>
 @endsection
