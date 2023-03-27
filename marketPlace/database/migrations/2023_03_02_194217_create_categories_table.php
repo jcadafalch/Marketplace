00c631296378d5ef57427eca2e37231c97698509
Daniel rodriguez saez <rodriguez.saez.daniel@alumnat.copernic.cat>
2023-03-27 16:33:12 +0200

    FIx : Solucionar problema de merge, no sabia passat, un camp de la migració de la taula categoria

--- marketPlace/database/migrations/2023_03_02_194217_create_categories_table.php
+++ marketPlace/database/migrations/2023_03_02_194217_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name')->unique()->nullable(false);
+            $table->bigInteger('parent_id')->nullable(true);
         });
     }
 
