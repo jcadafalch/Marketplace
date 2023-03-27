0f66955fc28a52451d46522a724ccfb5da6ea5bd
Daniel rodriguez saez <rodriguez.saez.daniel@alumnat.copernic.cat>
2023-03-27 20:31:01 +0200

    Afegir mètode per recuperar una tenda a través de l'usuari.

--- marketPlace/app/Models/User.php
+++ marketPlace/app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function shop()
+    {
+        return $this->hasOne(Shop::class);
+    }
 }
