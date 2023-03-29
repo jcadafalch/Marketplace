8a8d4e188f6806072b2a68cf6142d103a42276ec
Mariam Pipoyan <pipoyan.mariam@alumnat.copernic.cat>
2023-03-29 19:05:33 +0200

    Añadido funciones que permiten la recuperacion de contraseña al usuario

--- marketPlace/app/Http/Controllers/LogInController.php
+++ marketPlace/app/Http/Controllers/LogInController.php
@@ -2,8 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Password;
 
 class LogInController extends Controller
 {
@@ -12,11 +19,69 @@ class LogInController extends Controller
         return view('auth.login');
     }
 
-    public function recoveryPassword(){
+    public function recoveryPassword()
+    {
         return view('auth.recoveryPassword');
     }
 
-    public function createNewTenant(){
-        return view('tenant.createNewTenant',['categories' => Category::all()]);
+    public function recoveryPasswordSender(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            // |exists:users
+        ]);
+
+        $token = Str::random(64);
+
+        DB::table('password_reset_tokens')->insert([
+            'email' => $request->email,
+            'token' => $token,
+            'created_at' => Carbon::now()
+        ]);
+
+        Mail::send('email.forgetPassword', ['token' => $token], function ($message) use ($request) {
+            $message->to($request->email);
+            $message->subject('Reset Password');
+        });
+
+        return back()->with('message', 'We have e-mailed your password reset link!');
+    }
+
+    public function showResetPasswordForm($token)
+    {
+        return view('auth.forgetPasswordLink', ['token' => $token]);
+    }
+
+    public function submitResetPasswordForm(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            // |exists:users
+            'password' => 'required|string|min:6|confirmed',
+            'password_confirmation' => 'required'
+        ]);
+
+        $updatePassword = DB::table('password_reset_tokens')
+            ->where([
+                'email' => $request->email,
+                'token' => $request->token
+            ])
+            ->first();
+
+        if (!$updatePassword) {
+            return back()->withInput()->with('error', 'Invalid token!');
+        }
+
+        $user = User::where('email', $request->email)
+            ->update(['password' => Hash::make($request->password)]);
+
+        DB::table('password_reset_tokens')->where(['email' => $request->email])->delete();
+
+        return redirect('/login')->with('message', 'Your password has been changed!');
+    }
+    
+    public function createNewTenant()
+    {
+        return view('tenant.createNewTenant', ['categories' => Category::all()]);
     }
 }
