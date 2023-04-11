4083e9ca4b775b6d86f84ea66d38a01124ec9d42
Daniel rodriguez saez <rodriguez.saez.daniel@alumnat.copernic.cat>
2023-04-11 18:07:55 +0200

    Crear factoria per Shop

---
+++ marketPlace/database/factories/ShopFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Shop>
+ */
+class ShopFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $allUsersId = User::count();
+        $userId = rand(1, $allUsersId);
+        
+        return [
+            'name'=>fake()->unique()->name($gender = 'female'),
+            'shop_name'=>fake()->unique()->company(),
+            'nif'=>fake()->unique()->postcode(),
+            'user_id'=> $userId,
+        ];
+    }
+}
