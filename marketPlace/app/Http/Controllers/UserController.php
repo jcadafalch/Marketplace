3d95d13f31305a9ba111c0fbdb297521c38794b0
Mariam Pipoyan <pipoyan.mariam@alumnat.copernic.cat>
2023-04-12 15:38:33 +0200

    añadido funcion para cambiar imagen de usuario desde formulario

--- marketPlace/app/Http/Controllers/UserController.php
+++ marketPlace/app/Http/Controllers/UserController.php
@@ -4,10 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Http\FormRequest;
 
 class UserController extends Controller
 {
     public function profile(){
         return view('user.profile',['categories' => Category::all()->where('parent_id', '=', null)]);
     }
+
+    public function userProfile(){
+        return view('user.userProfile',['categories' => Category::all()->where('parent_id', '=', null)]);
+    }
+
+    public function editProfile(Request $request){
+
+        $id = 1; //Auth::user()->id;
+        $user = User::findOrFail($id);
+
+        $path = $request->file('profilePhoto')->storeAs('public/img', 'profileImg'.$id.'.jpg');
+
+        $user->path = $path;
+
+        $user->save();
+
+        return view('user.profile',['categories' => Category::all()->where('parent_id', '=', null)]);
+    }
+
+
 }
