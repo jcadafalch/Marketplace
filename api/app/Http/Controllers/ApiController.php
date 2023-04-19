a5ed479d1c7e27d529d915c576ebf219e59b33c4
Daniel rodriguez saez <rodriguez.saez.daniel@alumnat.copernic.cat>
2023-04-19 15:46:39 +0200

    Logica per genear imatges

--- api/app/Http/Controllers/ApiController.php
+++ api/app/Http/Controllers/ApiController.php
@@ -32,10 +32,47 @@ class ApiController extends Controller
     }
 
     public function createImage(Request $request){
-
+        
         $this->response["msg"] = "Creo una imagen";
         $this->response["status"] = 200;
         return response()->json($this->response);
+        /* V1 
+        header("Content-Type: image/png");
+        $im = @imagecreate(110, 20)
+        or die("Cannot Initialize new GD image stream");
+        $background_color = imagecolorallocate($im, 0, 0, 0);
+        $text_color = imagecolorallocate($im, 233, 14, 91);
+        imagestring($im, 1, 5, 5,  "A Simple Text String", $text_color);
+        imagepng($im);
+        return $im;
+        imagedestroy($im);*/
+        /* V2
+        // Create the size of image or blank image
+        $image = imagecreate(500, 300);
+  
+        // Set the vertices of polygon
+        $values = array(
+            50,  50,  // Point 1 (x, y)
+            50, 250,  // Point 2 (x, y)
+            250, 50,  // Point 3 (x, y)
+            250,  250 // Point 3 (x, y)
+        );
+        // Set the background color of image
+        $background_color = imagecolorallocate($image,  0, 153, 0);
+     
+        // Fill background with above selected color
+        imagefill($image, 0, 0, $background_color);
+   
+        // Allocate a color for the polygon
+        $image_color = imagecolorallocate($image, 255, 255, 255);
+     
+        // Draw the polygon
+        imagepolygon($image, $values, 4, $image_color);
+     
+        // Output the picture to the browser
+        header('Content-type: image/png');
+        $filaName = "prueba";
+        imagepng($image);*/
     }
 
     public function deleteImage(Request $request){
