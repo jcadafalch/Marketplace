e9f2d2924876a1ab0d4c147808d8e647ed7267f9
Mariam Pipoyan <pipoyan.mariam@alumnat.copernic.cat>
2023-04-20 16:16:54 +0200

    limitado numero de productos mostrados en landingPage

--- marketPlace/resources/views/landingPage.blade.php
+++ marketPlace/resources/views/landingPage.blade.php
@@ -11,11 +11,14 @@
                 <ul class="landing-card">
                     <div class="landing-info">
                         <h3 class="landing-title">{{ $titles[$i] }}</h3>
-                        <a id="{{$i}}" href="{{ route('landingPage.showAll',['id' => $i]) }}" ><button class="button-landing"> Ver todo </button></a>
+                        <a id="{{ $i }}" href="{{ route('landingPage.showAll', ['id' => $i]) }}"><button
+                                class="button-landing"> Ver todo </button></a>
                     </div>
                     @foreach ($products[$i] as $item)
-                        <a href="{{ route('product.show', ['id' => $item->id]) }}"><img class="landing-image"
-                                src="{{ asset('storage/img/' . $item->url) }}" /></a>
+                        @if ($i < 4)
+                            <a href="{{ route('product.show', ['id' => $item->id]) }}"><img class="landing-image"
+                                    src="{{ asset('storage/img/' . $item->url) }}" /></a>
+                        @endif
                     @endforeach
                 </ul>
             @endfor
