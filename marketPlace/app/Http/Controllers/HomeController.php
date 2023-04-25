767d32057e0683cf881f197b778c647f3bb9d533
Daniel rodriguez saez <rodriguez.saez.daniel@alumnat.copernic.cat>
2023-04-25 16:48:39 +0200

    Afegir redirect a producte no trobat. Eliminar valors de la cerca si Cliquem el logo de la web.

--- marketPlace/app/Http/Controllers/HomeController.php
+++ marketPlace/app/Http/Controllers/HomeController.php
@@ -14,13 +14,14 @@ class HomeController extends Controller
     public function index()
     {
         Paginator::defaultView('default');
+        // Eliminar variables de session 
+        session()->forget(['category', 'search']);  
 
         return view('home.index' , ['products' => Product::with('categories')->paginate(env('PAGINATE', 10))],['categories' => Category::all()->where('parent_id', '=', null)]);
     }
 
      public function show($id)
     {   
-        // dd(Shop::getShopNameByProductId(1));
 
         return view('home.singleProduct', ['product' => Product::findOrFail($id), 'shop' => Shop::getShopNameByProductId($id)], ['categories' => Category::all()->where('parent_id', '=', null)]);
     }
@@ -32,8 +33,8 @@ class HomeController extends Controller
         $order = $request['order'];
        
         // Guardem la categoria y la cerca a sessió  
-        $request->session()->put('category', $category);
-        $request->session()->put('search', $fieldSearch);
+        session()->put('category', $category);
+        session()->put('search', $fieldSearch);
 
         if( $request['category'] == 'allCategories'){
             $productsFilter = Product::searchByName($request);
@@ -41,7 +42,7 @@ class HomeController extends Controller
             $productsFilter = Product::searchByAll($request);
         }
         if($productsFilter->count() == 0){
-            $request->session()->flash('status','404 Not found!'); 
+           return redirect()->route('error.productNotFoundError');
         }
         return view('home.index', ['products' => $productsFilter->appends(['category' => $category,'search' => $fieldSearch, 'order' => $order])], ['categories' => Category::all()->where('parent_id', '=', null)]);
     }
