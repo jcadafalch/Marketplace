68cecba3d51a2240ab718a4fff47e111ba8e9034
Mariam Pipoyan <pipoyan.mariam@alumnat.copernic.cat>
2023-05-01 22:44:09 +0200

    añadido boton 'leer mas' en descripcion de la tienda

--- marketPlace/resources/views/shop/index.blade.php
+++ marketPlace/resources/views/shop/index.blade.php
@@ -11,13 +11,13 @@
         <article class="shop-info">
             <div class="shop-info-detail-shop">
                 <div class="shop-info-detail-shop-img">
-                    <img src="{{ asset('storage/img/profile/' . Auth::user()->path) }}" alt="Imagen de perfil">
+                    <img src="{{ asset('storage/img/profile/profileImg1.jpg') }}" alt="Imagen de perfil">
                 </div>
                 <p>Nombre de Tienda</p>
             </div>
             <div class="shop-info-detail-seller">
                 <div class="shop-info-detail-seller-img">
-                    <img src="{{ asset('storage/img/profile/' . Auth::user()->path) }}"
+                    <img src="{{ asset('storage/img/profile/profileImg1.jpg') }}"
                         onerror="this.src='{{ asset('storage/img/profile/defaultProfileImage.jpg') }}'"
                         alt="Imagen de perfil">
                 </div>
@@ -26,8 +26,19 @@
         </article>
         <article class="shop-description">
             <h4>Mensaje de la Tienda:</h4>
+            <input type="checkbox" id="expanded" name="expanded">
             <p>Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda
+                Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda
+                Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda
+                Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda
+                Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda
+                Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda
+                Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda
                 Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda Mensaje de la Tienda </p>
+            {{-- <label for="expanded" role="button">Leer más</label> --}}
+            <div class="shop-description-button">
+                <label for="expanded" role="button" data-more="Leer más..." data-less="Leer menos..."></label>
+            </div>
         </article>
         <article class="shop-products">
             <h4>Todos los productos</h4>
@@ -46,7 +57,7 @@
                                 <p class="product-price"> {{ $product->price }}€ </p>
                             </a>
                         </div>
-        
+
                         <input class="button-addToCart" type="button" value="Añadir" id="{{ $product->id }}">
                     </li>
                 @endforeach
