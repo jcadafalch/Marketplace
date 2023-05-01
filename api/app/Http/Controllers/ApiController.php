9f2edf33a2067ad6488d7282654f32b62f9afc96
Daniel rodriguez saez <rodriguez.saez.daniel@alumnat.copernic.cat>
2023-05-01 18:20:28 +0200

    Afegir seguretat a l'api, serà totalment privada.

--- api/app/Http/Controllers/ApiController.php
+++ api/app/Http/Controllers/ApiController.php
@@ -17,137 +17,181 @@ class ApiController extends Controller
 
     private $urlImage = "storage/img/";
 
-    public function getImage(Request $request){
-    Log::error("agafar un imatge");
-    
-       try {
-            //$file = Storage::disk('img')->get($request->name);
-            //$file = $file != null ? $file : "Imagen no encontrada"; 
 
-            $allFiles =  Storage::disk('img')->allFiles();
-            if(count($allFiles) > 0){
-                for ($i=0; $i < count($allFiles); $i++) { 
-                    if($allFiles[$i] == $request->name ){
-                     self::generateResponse("Cojo una imagen",200, $allFiles[$i]);
-                     break;
-                    }else{
-                     self::generateResponse("Imagen no encontrada", 404 . " Not Found", $request->name);
+    public function getImage(Request $request)
+    {
+        if ($request->bearerToken() == env('API_TOKEN')) {
+            Log::error("agafar un imatge");
+            try {
+
+                $allFiles =  Storage::disk('img')->allFiles();
+                if (count($allFiles) > 0) {
+                    
+                    for ($i = 0; $i < count($allFiles); $i++) {
+                        if ($allFiles[$i] == $request->name) {
+                            self::generateResponse("Cojo una imagen", 200, $allFiles[$i]);
+                            break;
+                        } else {
+                            self::generateResponse("Imagen no encontrada", 404 . " Not Found", $request->name);
+                        }
                     }
-                 }
-
-            }else{
-                self::generateResponse("No hay imagenes en la api", 500 . " Not Found", "Por favor genera alguna");
+                } else {
+                    self::generateResponse("No hay imagenes en la api", 500 . " Not Found", "Por favor genera alguna");
+                }
+            } catch (\Throwable $th) {
+                self::generateResponse("Imagen no encontrada", 404 . " Not Found", $request->name);
             }
-       } catch (\Throwable $th) {
-            self::generateResponse("Imagen no encontrada", 404 . " Not Found", $request->name);
-       }
-        return response()->json($this->response);
-    }
-
-    public function getAllImage(Request $request){
-        Log::error("Agafar totes les imatges");
-        try {
-            $allFiles =  Storage::disk('img')->allFiles();
-
-            if(count($allFiles) > 0){
-                self::generateResponse("Return all Files", 200, $allFiles);
-            }else{
-                self::generateResponse("No hay imagenes en la api", 404 . " Not Found", "Por favor genera alguna");
-            }          
-        } catch (\Throwable $th) {
-            self::generateResponse("failed to obtain all images", 500, "Contact technical service");
+        }else {
+            Log::error("Han intentado usar este recurso getImage(), sin Autorización");
+            self::generateResponse("No tienes acceso a este recurso", 401, "Unauthorized");
         }
         return response()->json($this->response);
     }
 
-    public function createImage(Request $request){
-        Log::error("Imagen creada");
-        try {
-            $fileName =  $request->ContextName;
-            $image = imagecreate(500, 300);
-            // Set the background color of image
-            $background_color = self::generateColors($image);
-        
-            // Fill background with above selected color
-            imagefill($image, 0, 0, $background_color);
-            
-            self::createPolygon($image);
-            self::generateText($image, $fileName);
-        
-            $photoName = self::saveImageToStorage($image);
-            imagedestroy($image);
-            self::generateResponse("Image create successful", 200, $photoName);
+    public function getAllImage(Request $request)
+    {
 
-        } catch (Throwable $th) {
-            self::generateResponse("Faild to create image", 500, $request->name);
-        }
-
-       return response()->json($this->response);
-    }
-
-    public function deleteImage(Request $request){
-        Log::error("imagen borrada" . $request->name);
-        try{      
-
-            Storage::disk('img')->delete($request->name);
-            //Log::error(Storage::disk('img')->exists($request->name));
-            self::generateResponse( "Imagen Borrada", 200, $request->name);
-        }catch (Throwable $th) {
-            self::generateResponse("Imagen no encontrada", 404 . " Not Found", $request->name);
-        }
-      
-        return response()->json($this->response);
-    }
-
-    public function deleteImageByProductName(Request $request){
-        Log::error("Imagenes de un producto borradas");
-        try {
-            $allImages = $request->all();
-
-            for ($i=0; $i < count($allImages); $i++) { 
-                Log::error("imagen eliminada:" . $allImages[$i]);
-                Storage::disk('img')->delete($allImages[$i]);
-            }
-            self::generateResponse("imagenes de un producto borradas", 200, $allImages);
-        } catch (\Throwable $th) {
-            self::generateResponse("Producto no encontrada", 404 . " Not Found", $allImages);
-        }
-           
-        return response()->json($this->response);
-    }
-
-    public function deleteAllImages(){
-        Log::error("Se han borrado todas las imagenes");
+        if ($request->bearerToken() == env('API_TOKEN')) {
+            Log::error("Agafar totes les imatges");
             try {
                 $allFiles =  Storage::disk('img')->allFiles();
-                for ($i=0; $i < count($allFiles) ; $i++) { 
-                    Storage::disk('img')->delete($allFiles[$i]);  
+
+                if (count($allFiles) > 0) {
+                    self::generateResponse("Return all Files", 200, $allFiles);
+                } else {
+                    self::generateResponse("No hay imagenes en la api", 404 . " Not Found", "Por favor genera alguna");
+                }
+            } catch (\Throwable $th) {
+                self::generateResponse("failed to obtain all images", 500, "Contact technical service");
+            }
+        } else {
+            Log::error("Han intentado usar este recurso getAllImage(), sin Autorización");
+            self::generateResponse("No tienes acceso a este recurso", 401, "Unauthorized");
+        }
+        return response()->json($this->response);
+    }
+
+    public function createImage(Request $request)
+    {
+
+        if ($request->bearerToken() == env('API_TOKEN')) {
+            Log::error("Imagen creada");
+            try {
+                $fileName =  $request->ContextName;
+                $image = imagecreate(500, 300);
+                // Set the background color of image
+                $background_color = self::generateColors($image);
+
+                // Fill background with above selected color
+                imagefill($image, 0, 0, $background_color);
+
+                self::createPolygon($image);
+                self::generateText($image, $fileName);
+
+                $photoName = self::saveImageToStorage($image);
+                imagedestroy($image);
+                self::generateResponse("Image create successful", 200, $photoName);
+            } catch (Throwable $th) {
+                self::generateResponse("Faild to create image", 500, $request->name);
+            }
+        } else {
+            Log::error("Han intentado usar este recurso createImage(), sin Autorización");
+            self::generateResponse("No tienes acceso a este recurso", 401, "Unauthorized");
+        }
+
+
+
+        return response()->json($this->response);
+    }
+
+    public function deleteImage(Request $request)
+    {
+
+        if ($request->bearerToken() == env('API_TOKEN')) {
+            Log::error("imagen borrada" . $request->name);
+            try {
+                Storage::disk('img')->delete($request->name);
+                //Log::error(Storage::disk('img')->exists($request->name));
+                self::generateResponse("Imagen Borrada", 200, $request->name);
+            } catch (Throwable $th) {
+                self::generateResponse("Imagen no encontrada", 404 . " Not Found", $request->name);
+            }
+        } else {
+            Log::error("Han intentado usar este recurso deleteImage(), sin Autorización");
+            self::generateResponse("No tienes acceso a este recurso", 401, "Unauthorized");
+        }
+
+        return response()->json($this->response);
+    }
+
+    public function deleteImageByProductName(Request $request)
+    {
+
+        if ($request->bearerToken() == env('API_TOKEN')) {
+            Log::error("Imagenes de un producto borradas");
+            try {
+                $allImages = $request->all();
+
+                for ($i = 0; $i < count($allImages); $i++) {
+                    Log::error("imagen eliminada:" . $allImages[$i]);
+                    Storage::disk('img')->delete($allImages[$i]);
+                }
+                self::generateResponse("imagenes de un producto borradas", 200, $allImages);
+            } catch (\Throwable $th) {
+                self::generateResponse("Producto no encontrada", 404 . " Not Found", $allImages);
+            }
+        } else {
+            Log::error("Han intentado usar este recurso deleteImageByProductName(), sin Autorización");
+            self::generateResponse("No tienes acceso a este recurso", 401, "Unauthorized");
+        }
+
+        return response()->json($this->response);
+    }
+
+    public function deleteAllImages(Request $request)
+    {
+
+        if ($request->bearerToken() == env('API_TOKEN')) {
+            Log::error("Se han borrado todas las imagenes");
+            try {
+                $allFiles =  Storage::disk('img')->allFiles();
+                for ($i = 0; $i < count($allFiles); $i++) {
+                    Storage::disk('img')->delete($allFiles[$i]);
                 }
                 self::generateResponse("Imagenes borradas correctamente", 200, "");
             } catch (\Throwable $th) {
-                self::generateResponse("Fallo al borrar las imagenes", 500 , "");
+                self::generateResponse("Fallo al borrar las imagenes", 500, "");
             }
+        } else {
+            Log::error("Han intentado usar este recurso deleteAllImages(), sin Autorización");
+            self::generateResponse("No tienes acceso a este recurso", 401, "Unauthorized");
+        }
 
-            return response()->json($this->response);
+        return response()->json($this->response);
     }
 
-    public function pushImage(Request $request){
+    public function pushImage(Request $request)
+    {
         /*$request->validate([
             'image' => 'required|image|mimes:png,jpg,gif,svg|max:2048',
         ]);*/
+        if ($request->bearerToken() == env('API_TOKEN')) {
+            try {
+                Log::error("Imagen subida");
+                $urlServer = "http://" . $_SERVER['HTTP_HOST'] . "/";
+                $RandomNameImage = uniqid() . "." . $request["imagen"]->extension();
 
-        try {
-            Log::error("Imagen subida");
-            $RandomNameImage = uniqid() . "." . $request["imagen"]->extension();
-        
-            //$request["imagen"]->store('img');
-            Storage::disk('img')->put($RandomNameImage ,$request["imagen"]->get());
-            Log::error("Imatge pujada al servidor at:");
-            self::generateResponse("Subo una imagen", 200, $RandomNameImage);
-        } catch (\Throwable $th) {
-            self::generateResponse("Fallo al subir la imagen", 200, "Suba otra vez la imagen");
+                Storage::disk('img')->put($RandomNameImage, $request["imagen"]->get());
+                Log::error("Imatge pujada al servidor at:");
+                self::generateResponse("Subo una imagen", 200,$urlServer . $this->urlImage . $RandomNameImage);
+            } catch (\Throwable $th) {
+                self::generateResponse("Fallo al subir la imagen", 200, "Suba otra vez la imagen");
+            }
+        } else {
+            Log::error("Han intentado usar este recurso deleteAllImages(), sin Autorización");
+            self::generateResponse("No tienes acceso a este recurso", 401, "Unauthorized");
         }
-      
+
         return response()->json($this->response);
     }
 
@@ -164,24 +208,27 @@ class ApiController extends Controller
      * function in PHP. The background color is generated randomly using three integers between 0 and
      * 255, which represent the red, green, and blue color values.
      */
-    public function generateColors($image){
-          
-          $color1 = random_int( 0, 255);
-          $color2 = random_int( 0, 255);
-          $color3 = random_int( 0, 255);
+    public function generateColors($image)
+    {
 
-       return $background_color = imagecolorallocate($image, $color1, $color2, $color3);
+        $color1 = random_int(0, 255);
+        $color2 = random_int(0, 255);
+        $color3 = random_int(0, 255);
+
+        return $background_color = imagecolorallocate($image, $color1, $color2, $color3);
     }
 
-    
-    public function generateText($image, $fileName){
+
+    public function generateText($image, $fileName)
+    {
         $font = 'arial.ttf';
         $black = imagecolorallocate($image, 0, 0, 0);
         imagettftext($image, 20, 0, 200, 170, $black, $font, $fileName);
     }
 
 
-    public function createPolygon($image){
+    public function createPolygon($image)
+    {
         $values = array(
             50,  50,  // Point 1 (x, y)
             50, 250,  // Point 2 (x, y)
@@ -190,22 +237,24 @@ class ApiController extends Controller
         );
 
         $image_color = imagecolorallocate($image, 255, 255, 255);
-        imagepolygon($image, $values, 4, $image_color);   
+        imagepolygon($image, $values, 4, $image_color);
     }
 
-    public function saveImageToStorage($image){
+    public function saveImageToStorage($image)
+    {
         $urlServer = "http://" . $_SERVER['HTTP_HOST'] . "/";
         $RandomNameImage = uniqid() . ".png";
         $path = $this->urlImage . $RandomNameImage;
-       
-        imagepng($image,$path);
-        
+
+        imagepng($image, $path);
+
         return $urlServer . $this->urlImage .  $RandomNameImage;
     }
 
-    public function generateResponse($mensaje, $status, $filename){
+    public function generateResponse($mensaje, $status, $filename)
+    {
         $this->response["msg"] = $mensaje;
         $this->response["status"] = $status;
         $this->response["filename"] = $filename;
-    }           
+    }
 }
