bd0e97f50b62e4ae4168a38306009e46e3b3b797
Mariam Pipoyan <pipoyan.mariam@alumnat.copernic.cat>
2023-05-02 11:21:11 +0200

    añadido previsualización de imagen de perfil al subirlo desde formulario 'Modificar Perfil'

--- marketPlace/resources/views/user/userProfile.blade.php
+++ marketPlace/resources/views/user/userProfile.blade.php
@@ -3,20 +3,19 @@
 @section('title', 'Perfil')
 
 @section('content')
-    <div class="profile">
-        <div class="user-content">
+    <section class="profile">
+        <article class="user-content">
             <div class="user-img">
-                <img src="{{ asset('storage/img/profile/' . Auth::user()->path) }}" onerror="this.src='{{ asset('storage/img/profile/defaultProfileImage.jpg') }}'" alt="Imagen de perfil">
+                <img src="{{ asset('storage/img/profile/' . Auth::user()->path) }}"
+                    onerror="this.src='{{ asset('storage/img/profile/defaultProfileImage.jpg') }}'" alt="Imagen de perfil">
             </div>
-
             <div class="user-info">
-                <p class="user-name">{{Auth::user()->name}}</p>
+                <p class="user-name">{{ Auth::user()->name }}</p>
 
                 <a href={{ route('user.profile') }}><button class="button-changeProfile">Modificar perfil</button></a>
                 <a href={{ route('shop.createNewShop') }}><button class="button-newShop">Mi tienda</button></a>
             </div>
+        </article>
+    </section>
 
-        </div>
-
-
-    @endsection
+@endsection
