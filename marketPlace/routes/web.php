5ea5feffcfdef4fa0a4c20f5cd03efa4fe7087a8
Mariam Pipoyan <pipoyan.mariam@alumnat.copernic.cat>
2023-05-03 00:22:03 +0200

    añadido rutas de nuevas paginas: tienda, añadirProducto, editarTienda

--- marketPlace/routes/web.php
+++ marketPlace/routes/web.php
@@ -89,3 +89,4 @@ Route::get('/administrarTenda/{id}', [ManageShopController::class, 'index'])->na
 //new
 Route::get('/tienda', [ShopController::class, 'index'])->name('shop.show');
 Route::get('/añadirProducto', [ShopController::class, 'newProduct'])->name('shop.newProduct');
+Route::get('/tienda/editar', [ShopController::class, 'edit'])->name('shop.edit');
