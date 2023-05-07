fe5b7253c4c867ea01b219d7e163316def91329a
Daniel rodriguez saez <rodriguez.saez.daniel@alumnat.copernic.cat>
2023-05-07 20:48:25 +0200

    JavaScript amb Asincronia, per actualitzar l'estat dels productes.

---
+++ marketPlace/public/js/editProducts.js
@@ -0,0 +1,77 @@
+/**
+ * Selectors.
+ */
+const buttonDissable = document.querySelectorAll(".dissable");
+const buttonAble = document.querySelectorAll(".able");
+const buttonDelete = document.querySelectorAll(".delete");
+
+
+/**
+ * Funció async que fa la petició a l'url, per actualitzar l'estat del producte.
+*/
+const ShopEditProduct = async (id, action) => {
+    try {
+        const response = await fetch(`/tienda/editarProducto/?id=${id}&action=${action}`);
+        response;
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        return null;
+    }
+};
+
+/**
+ * EventListener per el butó Habilitar
+*/
+buttonAble.forEach((elementAction) => {
+   
+    elementAction.addEventListener("click", function (e) {
+        console.log(elementAction.id); 
+        console.log(elementAction.className); 
+        ShopEditProduct(parseInt(elementAction.id), elementAction.className).then((res) => {
+            if (res !== true) {
+                return;
+            }
+            return;
+        });
+    });
+});
+
+/**
+ * EventListener per el butó eliminar
+*/
+buttonDelete.forEach((elementAction) => {
+   
+    elementAction.addEventListener("click", function (e) {
+        console.log('entro Delete');
+        console.log(elementAction.id); 
+        console.log(elementAction.className); 
+        ShopEditProduct(parseInt(elementAction.id, ), elementAction.className).then((res) => {
+            if (res !== true) {
+                return;
+            }
+            return;
+        });
+    });
+});
+
+/**
+ * EventListener per el butó Deshabilitar
+*/
+buttonDissable.forEach((elementAction) => {
+    elementAction.addEventListener("click", function (e) {
+        console.log('entro Dissable');
+        console.log(elementAction.id); 
+        console.log(elementAction.className); 
+        ShopEditProduct(parseInt(elementAction.id), elementAction.className).then((res) => {
+            if (res !== true) {
+                return;
+            }
+            return;
+        });
+    });
+});
