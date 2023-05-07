6a0b34a6e448c30771fee3d39f7a1fec7f628a78
Daniel rodriguez saez <rodriguez.saez.daniel@alumnat.copernic.cat>
2023-05-07 15:56:14 +0200

    Request per EditShop

---
+++ marketPlace/app/Http/Requests/ShopEdit.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ShopEdit extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'shopName' => 'required|min:5',
+            'name' => 'required|max:30',
+            'nif' =>  ['required', 'regex:/^\d{8,8}[a-zA-Z]$/'],
+            'profilePhoto' => 'required|image|mimes:png,jpg,jpeg'
+         ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'required' => 'El campo :attribute no puede estar vacio.',
+            'min' => [ 'string' => 'El campo :attribute tiene que tener como minimo :min caracteres.',
+            ],
+            'max' => ['string' => 'El nombre del propietario no puede superar :max caracteres'
+            ],
+            'image' => 'Solo se pueden cargar imagenes.',
+            'mimes' => 'Formatos admtidos: png, jpg, jpeg.',
+            'regex' => 'Formato de Dni incorrecto.'
+        ];
+    }
+
+    public function attributes()
+    {
+    return [
+        'shopName' => 'Nombre de la tienda',
+        'name' => 'Nombre del Propietario',
+        'nif' => 'Nif o Dni',
+        'profilePhoto' => 'Foto de Perfil'
+    ];
+}
+}
