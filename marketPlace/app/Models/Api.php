01a1134273b706f17deef1e80e042dff733c83fd
Daniel rodriguez saez <rodriguez.saez.daniel@alumnat.copernic.cat>
2023-05-09 20:34:02 +0200

    Acabar metodos para poder usar los endpoints de la APi. Falta el de subir imagenes.

--- marketPlace/app/Models/Api.php
+++ marketPlace/app/Models/Api.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Api extends Model
 {
     use HasFactory;
-    
-    private  $ApiUrl = "http://127.0.0.1:8000/api/";
+
+    public  $ApiUrl = "http://127.0.0.1:8000/api/";
 
     private $endpointCreateImage = "createImage";
     private $endpointDeleteImage = "deleteImage";
@@ -17,40 +19,102 @@ class Api extends Model
     private $endpointDeleteAllImageProduct = "deleteImageByProductName";
     private $endpointDeleteAllImageApi = "deleteAllImages";
 
-    public static function createImage(){
-    
-    $url = $this->ApiUrl . $this->endpointCreateImage;
-        
+    // Funciona
+    public function createImage($ImageName)
+    {
+       
+        $url = $this->ApiUrl . $this->endpointCreateImage;
+
+        $response = Http::withToken(env('API_TOKEN'))
+        ->get($url, ['ContextName' => $ImageName]);
+
+
+        if ($response->ok()) {
+
+            return $response['filename'];
+        } else {
+            return $response->json();;
+        }
     }
 
-    public static function deleteImage(){
-    
-    $url = $this->ApiUrl . $this->endpointDeleteImage;
+     // Funciona
+    public function deleteImage($ImageName)
+    {
 
+        $url = $this->ApiUrl . $this->endpointDeleteImage;
+        $response = Http::withToken(env('API_TOKEN'))
+            ->delete($url, ['name' => $ImageName]);
 
+        if ($response->ok()) {
 
+            return $response->json();
+        } else {
+            return $response->json();;
+        }
     }
 
-    public static function pushImage(){
+    public function pushImage($imageContent)
+    {
 
-    $url = $this->ApiUrl . $this->endpointPushImage;
+        $url = $this->ApiUrl . $this->endpointPushImage;
 
+        $response = Http::withToken(env('API_TOKEN'))
+            ->withHeaders([
+                'Content-Type' => 'image/png',
+            ])
+            ->attach('imagen', $imageContent, 'image')
+            ->post($url);
+
+
+        if ($response->ok()) {
+
+            return $response->json();
+        } else {
+            return $response->json();;
+        }
     }
 
-    public static function deleteAllImagesProduct(){
-     
-    $url = $this->ApiUrl . $this->endpointDeleteAllImageProduct;
+    // Funciona
+    public function deleteAllImagesProduct($productsID)
+    {
+        $url = $this->ApiUrl . $this->endpointDeleteAllImageProduct;
+
+        $products =[
+            0 => "645a7c4de3bd8.png",
+            1 => "645a7c4ed6328.png",
+            2 => "645a7c8780996.png",
+            3 => "645a78d4cb2fc.png"
+        ];
 
 
 
+        $response = Http::withToken(env('API_TOKEN'))
+            ->withHeaders([
+                'Content-Type' => 'application/json',
+            ])
+            ->withBody(json_encode($products), 'application/json')
+            ->delete($url);
+
+        if ($response->ok()) {
+            return $response->json();
+        } else {
+            return $response->json();
+        }
     }
 
-    public static function deleteAllImages(){
+     // Funciona
+    public function deleteAllImages()
+    {
 
-    $url = $this->ApiUrl . $this->endpointDeleteAllImageApi;
+        $url = $this->ApiUrl . $this->endpointDeleteAllImageApi;
 
-    
+        $response = Http::withToken(env('API_TOKEN'))->delete($url);
 
+        if ($response->ok()) {
+           return $response->json();
 
-    }
+        } else {
+            return $response->json();
+        }
+        }
 }
