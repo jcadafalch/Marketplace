8b1551d7f02d9cf350ba405722692b8f39d76988
Mariam Pipoyan <pipoyan.mariam@alumnat.copernic.cat>
2023-05-10 03:43:48 +0200

    modificado estilos para adaptarlos a los cambios anteriores

--- marketPlace/resources/views/user/userProfile.blade.php
+++ marketPlace/resources/views/user/userProfile.blade.php
@@ -18,13 +18,13 @@
             <div class="user-info">
                 <p class="user-name">{{ Auth::user()->name }}</p>
 
-                <a href={{ route('user.profile') }}><button class="button-changeProfile">Modificar perfil</button></a>
+                <a href={{ route('user.profile') }}><button class="button-UserProfile">Modificar perfil</button></a>
                 @if ($shop != null)
-                    <a href={{ route('shop.show', ['shopName' => $shop->name]) }}><button class="button-newShop">Mi
+                    <a href={{ route('shop.show', ['shopName' => $shop->name]) }}><button class="button-UserProfile">Mi
                             tienda</button></a>
-                    <a href={{ route('shop.edit') }}><button class="button-newShop">Editar mi tienda</button></a>
+                    <a href={{ route('shop.edit') }}><button class="button-UserProfile">Editar mi tienda</button></a>
                 @else
-                    <a href={{ route('shop.createNewShop') }}><button class="button-newShop">Mi tienda</button></a>
+                    <a href={{ route('shop.createNewShop') }}><button class="button-UserProfile">Mi tienda</button></a>
                 @endif
             </div>
         </article>
